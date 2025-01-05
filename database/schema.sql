bafb521bc3d131f75bd1ca27b1dde1ada890b2d2
Viraj Aryans <127313631+AryansVj@users.noreply.github.com>
2025-01-05 07:16:10 +0530

    Error fixes

--- database/schema.sql
+++ database/schema.sql
@@ -4,36 +4,36 @@ USE AssignmentTracker;
 CREATE TABLE Roles (
     role_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     role_title VARCHAR(128),
-    PRIMARY KEY(role_id),
+    PRIMARY KEY(role_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE Groups (
     group_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    group VARCHAR(128),
-    PRIMARY KEY(group_id),
+    group_name VARCHAR(128),
+    PRIMARY KEY(group_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE People (
     person_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(128),
-    role_id INT,
-    group_id INT,
+    role_id INT UNSIGNED,
+    group_id INT UNSIGNED,
     PRIMARY KEY(person_id),
     INDEX USING BTREE (name),
 
-    CONSTRAINT FOREIGN KEY(role_id) REFERENCES Roles (role_id) ON DELETE CASCADE ON UPDATE CASCADE    
+    CONSTRAINT FOREIGN KEY(role_id) REFERENCES Roles (role_id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT FOREIGN KEY(group_id) REFERENCES Groups (group_id) ON DELETE CASCADE ON UPDATE CASCADE    
 ) ENGINE = InnoDB;
 
 CREATE TABLE AssignmentCategories (
     category_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     category_title VARCHAR(128),
-    PRIMARY KEY (category_id),
+    PRIMARY KEY (category_id)
 )ENGINE = InnoDB ;
 
 CREATE TABLE PerformanceLevels (
     performace_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    levels VARCHAR(128);
+    levels VARCHAR(128),
     PRIMARY KEY (performace_id)
 ) ENGINE = InnoDB;
 
@@ -52,12 +52,12 @@ CREATE TABLE Status (
 
 CREATE TABLE WeeklyTracker (
     assignment_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    person_id INT,
-    category_id INT,
-    assistant_id INT,
-    week_id INT,
-    status_id INT,
-    performace_id INT,
+    person_id INT UNSIGNED,
+    category_id INT UNSIGNED,
+    assistant_id INT UNSIGNED,
+    week_id INT UNSIGNED,
+    status_id INT UNSIGNED,
+    performace_id INT UNSIGNED,
     hall INT,
 
     PRIMARY KEY (assistant_id),
@@ -66,6 +66,6 @@ CREATE TABLE WeeklyTracker (
     CONSTRAINT FOREIGN KEY(category_id) REFERENCES AssignmentCategories(category_id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT FOREIGN KEY(assignment_id) REFERENCES People(person_id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT FOREIGN KEY(week_id) REFERENCES Weeks(week_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FOREIGN KEY(status_id) REFERENCES Status(status_id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT FOREIGN KEY(status_id) REFERENCES Status(status_id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT FOREIGN KEY(performace_id) REFERENCES PerformanceLevels(performace_id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB ;
