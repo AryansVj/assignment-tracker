c5b21801ea3b3a616549590431dedc66b8ef278d
Viraj Aryans <127313631+AryansVj@users.noreply.github.com>
2025-01-06 14:18:37 +0530

    getAllIndividual() added to model:assignment

--- model/Assignment.php
+++ model/Assignment.php
@@ -32,10 +32,22 @@ class Assignment {
         $query = "SELECT Person.name AS person_name, AssignmentCategories.category_title, Assistant.name AS assistant_name, Status.status_descriptor, PerformanceLevels.levels, WeeklyTracker.hall, Weeks.weekly_date
         FROM WeeklyTracker 
         JOIN People AS Person ON WeeklyTracker.person_id = Person.person_id JOIN AssignmentCategories ON WeeklyTracker.category_id = AssignmentCategories.category_id JOIN People AS Assistant ON WeeklyTracker.assistant_id = Assistant.person_id JOIN Status ON WeeklyTracker.status_id = Status.status_id JOIN PerformanceLevels ON WeeklyTracker.performace_id = PerformanceLevels.performace_id JOIN Weeks ON WeeklyTracker.week_id = Weeks.week_id 
-        WHERE YEAR(Weeks.weekly_date) = ? AND MONTH(Weeks.weekly_date) = ? ORDER BY Person.name;";
+        WHERE YEAR(Weeks.weekly_date) = ? AND MONTH(Weeks.weekly_date) = ? ORDER BY Weeks.weekly_date;";
 
         $stmt = $this->conn->prepare($query);
         $stmt->execute([$year, $month]);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllIndividual($person_name) {
+        $query = "SELECT AssignmentCategories.category_title, Assistant.name AS assistant_name, Status.status_descriptor, PerformanceLevels.levels, WeeklyTracker.hall, Weeks.weekly_date
+        FROM WeeklyTracker 
+        JOIN People AS Person ON WeeklyTracker.person_id = Person.person_id JOIN AssignmentCategories ON WeeklyTracker.category_id = AssignmentCategories.category_id JOIN People AS Assistant ON WeeklyTracker.assistant_id = Assistant.person_id JOIN Status ON WeeklyTracker.status_id = Status.status_id JOIN PerformanceLevels ON WeeklyTracker.performace_id = PerformanceLevels.performace_id JOIN Weeks ON WeeklyTracker.week_id = Weeks.week_id 
+        WHERE Person.name = ? ORDER BY Weeks.weekly_date;";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$person_name]);
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
