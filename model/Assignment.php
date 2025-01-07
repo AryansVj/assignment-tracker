8b7797cdb0b509adea2049a84f4123d51d1d8fc3
Viraj Aryans <127313631+AryansVj@users.noreply.github.com>
2025-01-07 14:29:11 +0530

    Error handling with try catch

--- model/Assignment.php
+++ model/Assignment.php
@@ -34,8 +34,14 @@ class Assignment {
         JOIN People AS Person ON WeeklyTracker.person_id = Person.person_id JOIN AssignmentCategories ON WeeklyTracker.category_id = AssignmentCategories.category_id JOIN People AS Assistant ON WeeklyTracker.assistant_id = Assistant.person_id JOIN Status ON WeeklyTracker.status_id = Status.status_id JOIN PerformanceLevels ON WeeklyTracker.performace_id = PerformanceLevels.performace_id JOIN Weeks ON WeeklyTracker.week_id = Weeks.week_id 
         WHERE YEAR(Weeks.weekly_date) = ? AND MONTH(Weeks.weekly_date) = ? ORDER BY Weeks.weekly_date;";
 
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute([$year, $month]);
+        try {
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$year, $month]);
+        }
+        catch (PDOException $e) {
+            echo 'Exception occured. Error code: ' . $e->getCode(); 
+            echo '<br>Error Message: ' . $e->getMessage();
+        }
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
