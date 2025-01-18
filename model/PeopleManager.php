0a977295fdf09a73f7e01392cb0bea809b3417a0
Viraj Aryans <127313631+AryansVj@users.noreply.github.com>
2025-01-18 23:32:46 +0530

    Functions to get pp assig count and person info

--- model/PeopleManager.php
+++ model/PeopleManager.php
@@ -35,4 +35,38 @@ class People {
             return -1;
         }
     }
+
+    public function getPersonInfo($person_name) {
+        $query = "SELECT People.name, People.person_id, Roles.role_title, Groups.group_name FROM People JOIN Roles ON People.role_id = Roles.role_id JOIN Groups ON People.group_id = Groups.group_id WHERE People.name = ?";
+
+        try {
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$person_name]);
+        } catch (PDOException $e) {
+            echo 'Exception occured. Error code: ' . $e->getCode(); 
+            echo '<br>Error Message: ' . $e->getMessage();
+
+            return -1;
+        }
+
+        return $stmt->fetch();
+    }
+
+    public function getAssignmentCount($person_name) {
+        // Get Person wise count of total assignments
+        $query = "SELECT COUNT(assignment_id) AS assignment_count FROM WeeklyTracker JOIN People ON WeeklyTracker.person_id = People.person_id WHERE People.name = ?";
+
+        try {
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$person_name]);
+        } catch (PDOException $e) {
+            echo 'Exception occured. Error code: ' . $e->getCode(); 
+            echo '<br>Error Message: ' . $e->getMessage();
+
+            return -1;
+        }
+
+        $res = $stmt->fetch();
+        return $res['assignment_count'];
+    }
 }
