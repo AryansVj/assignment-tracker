75a2cbd9d21e0ff5e59a288b9c78044541a947ab
Viraj Aryans <127313631+AryansVj@users.noreply.github.com>
2025-01-20 14:10:48 +0530

    Search added to people manage page

--- ManagePerson.php
+++ ManagePerson.php
@@ -45,6 +45,21 @@ if ( isset($_POST['Add']) && isset($_POST['Name']) && ($_POST['role'] != 0) && (
     return;
 } 
 
+// Search section using GET
+if ( isset($_GET['search']) ) {
+    if ( isset($_GET['search-name']) || ($_GET['search-role'] > 0) || ($_GET['search-group'] > 0) || ($_GET['search-responsibility'] > 0) ) {
+        $search = $people->searchPeople($_GET['search-name'], $_GET['search-role'] + 0, $_GET['search-group'] + 0, $_GET['search-responsibility'] + 0);
+        if ($search == -1) {
+            $_SESSION['error'] = "Search failed!";
+        } elseif (sizeof($search) == 0) {
+            $_SESSION['error'] = "No results found for the given query";
+        } 
+    }
+} elseif ( isset($_GET['clear']) ) {
+    header("Location: ManagePerson.php");
+    return;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -62,58 +77,117 @@ if ( isset($_POST['Add']) && isset($_POST['Name']) && ($_POST['role'] != 0) && (
         <a class="people" href="ManagePerson.php">People</a>
         <a class="weeks" href="SelectWeek.php">Weeks</a>
     </div>
-    <h1>Add a New Person</h1>
-    <div class="form-container">
-        <!-- Log Message Section -->
-        <?php if (isset($_SESSION['error'])): ?>
-            <div class="message error" id="log-message">
-                <?= htmlentities($_SESSION['error']) ?>
-            </div>
-        <?php unset($_SESSION['error']); endif; ?>
+    <h1>Manage People</h1>
+    <div class="container">
+        <div class="form-container">
+            <!-- Log Message Section -->
+            <?php if (isset($_SESSION['error'])): ?>
+                <div class="message error" id="log-message">
+                    <?= htmlentities($_SESSION['error']) ?>
+                </div>
+            <?php unset($_SESSION['error']); endif; ?>
 
-        <?php if (isset($_SESSION['success'])): ?>
-            <div class="message success" id="log-message">
-                <?= htmlentities($_SESSION['success']) ?>
-            </div>
-        <?php unset($_SESSION['success']); endif; ?>
+            <?php if (isset($_SESSION['success'])): ?>
+                <div class="message success" id="log-message">
+                    <?= htmlentities($_SESSION['success']) ?>
+                </div>
+            <?php unset($_SESSION['success']); endif; ?>
 
-        <!-- Form Section -->
-        <form method="post">
-            <p>
-                <label for="Name">Enter the Name</label>
-                <input type="text" id="Name" name="Name" placeholder="Name" required>
-            </p>
-            <p>
-                <label for="role">Role Title</label>
-                <select name="role" id="role" required>
-                    <option value="0" selected>Select Role</option>
-                    <option value="1">Unbaptized Publisher</option>
-                    <option value="2">Baptized Publisher</option>
-                    <option value="3">Auxiliary Pioneer</option>
-                    <option value="4">Regular Pioneer</option>
-                    <option value="5">Special Pioneer</option>
-                </select>
-            </p>
-            <p>
-                <label for="group">Group</label>
-                <select name="group" id="group" required>
-                    <option value="0" selected>Select Group</option>
-                    <option value="1">Kundasale</option>
-                    <option value="2">Digana</option>
-                </select>
-            </p>
-            <p>
-                <label for="responsibility">Responsibility</label><br>
-                <input type="radio" name="responsibility" value="1" checked> None
-                <input type="radio" name="responsibility" value="2"> Ministerial Servant
-                <input type="radio" name="responsibility" value="3"> Elder
-            </p>
-            <div class="button-container">
-                <input class="add" type="submit" name="Add" value="Add Person">
-                <input class="delete" type="submit" name="Delete" value="Delete">
-                <a href="index.php" class="btn-link">Home</a>
-            </div>
-        </form>
+            <!-- Form Section -->
+            <form method="post" class="add">
+                <p>
+                    <label for="Name">Enter the Name</label>
+                    <input type="text" id="Name" name="Name" placeholder="Name" required>
+                </p>
+                <p>
+                    <label for="role">Role Title</label>
+                    <select name="role" id="role" required>
+                        <option value="0" selected>Select Role</option>
+                        <option value="1">Unbaptized Publisher</option>
+                        <option value="2">Baptized Publisher</option>
+                        <option value="3">Auxiliary Pioneer</option>
+                        <option value="4">Regular Pioneer</option>
+                        <option value="5">Special Pioneer</option>
+                    </select>
+                </p>
+                <p>
+                    <label for="group">Group</label>
+                    <select name="group" id="group" required>
+                        <option value="0" selected>Select Group</option>
+                        <option value="1">Kundasale</option>
+                        <option value="2">Digana</option>
+                    </select>
+                </p>
+                <p>
+                    <label for="responsibility">Responsibility</label><br>
+                    <input type="radio" name="responsibility" value="1" checked> None
+                    <input type="radio" name="responsibility" value="2"> Ministerial Servant
+                    <input type="radio" name="responsibility" value="3"> Elder
+                </p>
+                <div class="button-container">
+                    <input class="add" type="submit" name="Add" value="Add Person">
+                    <input class="delete" type="submit" name="Delete" value="Delete">
+                    <a href="index.php" class="btn-link">Home</a>
+                </div>
+            </form>
+        </div>
+
+        <div class="display-container">
+            <form class="search" method="get">
+                <p>
+                    <input type="text" name="search-name" placeholder="Enter name to search">
+                </p>
+                <div class="search-select">
+                    <p>
+                        <select name="search-role" id="search-role" required>
+                            <option value="0" selected>Role</option>
+                            <option value="1">Unbaptized Publisher</option>
+                            <option value="2">Baptized Publisher</option>
+                            <option value="3">Auxiliary Pioneer</option>
+                            <option value="4">Regular Pioneer</option>
+                            <option value="5">Special Pioneer</option>
+                        </select>
+                    </p>
+                    <p>
+                        <select name="search-group" id="search-group" required>
+                            <option value="0" selected>Group</option>
+                            <option value="1">Kundasale</option>
+                            <option value="2">Digana</option>
+                        </select>
+                    </p>
+                    <p>
+                        <select name="search-responsibility" id="search-responsibility">
+                            <option value="0" selected>Responsibility</option>
+                            <option value="1">Brother/Sister</option>
+                            <option value="2">Ministerial Servant</option>
+                            <option value="3">Elder</option>
+                        </select>
+                    </p>
+                    <input class="clear" type="submit" name="clear" value="Clear">
+                    <input class="search" type="submit" name="search" value="Search">
+                </div>    
+            </form>
+            <hr>
+            <?php if (isset($_GET['search'])): ?>
+                <div class="display">
+                    <?php foreach($search as $row): ?>
+                        <div class="search-row">
+                            <p>
+                                <strong><?= $row['name']?></strong><br>
+                                <?= $row['role_title']?>, <?= $row['group_name']?>
+                            </p>
+                            <?php if ($row['responsibility'] != "None"): ?>
+                                <p class="resp"><?= $row['responsibility']?></p>
+                            <?php endif;?>
+                            <div class="edit">
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </div>
+                        </div>
+                    <?php endforeach;?>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
 </body>
 </html>
